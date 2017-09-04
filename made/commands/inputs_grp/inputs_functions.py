64de1a73c45d86cc2c084d67b96b2acba8004abb
Enda Ridge <guerrillaanalytics@gmail.com>
2017-09-04 21:42:25 +0100

    Added input path creation functionality

--- made/commands/inputs_grp/inputs_functions.py
+++ made/commands/inputs_grp/inputs_functions.py
@@ -1,3 +1,25 @@
+import errno
+import os
+
+
+def input_build_name(source_id, source_label, version, schema):
+    """create a folder path for a given input"""
+
+    # TODO Check source_id does not contain spaces
+
+    path = os.path.join(source_id.lower() + "_" + source_label.lower(),
+                        version.zfill(3), schema)
+    return path
+
+
 def input_create(source_id, source_label, schema, version):
     """Creates an input folder tree in the correct structure"""
+    path = input_build_name(source_id, source_label, version, schema)
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
     pass
