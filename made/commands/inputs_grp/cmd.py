62b809f5d94a29b0f5c15a6dd274f79cc3c4b6b5
Enda Ridge <guerrillaanalytics@gmail.com>
2017-09-07 20:22:22 +0100

    error checking at command prompt for input create

--- made/commands/inputs_grp/cmd.py
+++ made/commands/inputs_grp/cmd.py
@@ -16,9 +16,29 @@ def input(ctx):
 @click.pass_obj
 def input_create(ctx):
     user_root = os.getcwd()
-    user_source_id = click.prompt('Please enter an input ID', type=str)
-    user_source_name = click.prompt('Please enter a schema name to load into', type=str)
-    user_version = click.prompt('Please enter a version', type=int)
+
+    while True:
+        user_source_id = click.prompt('Please enter an input ID', type=str)
+        if " " in user_source_id:
+            continue
+        break
+
+    # source name
+    while True:
+        user_source_name = click.prompt('Please enter a schema name to load into', type=str)
+        if len(user_source_name.strip()) == 0:
+            continue
+        if " " in user_source_name.strip():
+            continue
+        break
+
+    # version of input
+    while True:
+        user_version = click.prompt('Please enter a version', type=int, default=1)
+        if user_version <= 0:
+            continue
+        break
+
     input_functions.input_create(root_folder=user_root, source_id=user_source_id, source_name=user_source_name,
                                  version=user_version)
     pass
