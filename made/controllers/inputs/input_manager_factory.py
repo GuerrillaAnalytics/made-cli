b2bf4423134819e533e7d9fbba85661f0de2be83
Enda Ridge <guerrillaanalytics@gmail.com>
2018-02-04 19:56:15 +0000

    more work to do

--- made/controllers/inputs/input_manager_factory.py
+++ made/controllers/inputs/input_manager_factory.py
@@ -120,6 +120,62 @@ class S3InputManager(InputManager):
     Class for managing inputs folders on S3
     """
 
+    def get_matching_s3_objects(self, bucket, prefix='', suffix=''):
+        """
+        Generate objects in an S3 bucket.
+
+        :param bucket: Name of the S3 bucket.
+        :param prefix: Only fetch objects whose key starts with
+            this prefix (optional).
+        :param suffix: Only fetch objects whose keys end with
+            this suffix (optional).
+        From https://alexwlchan.net/2018/01/listing-s3-keys-redux/
+        """
+        s3 = boto3.client('s3')
+        kwargs = {'Bucket': bucket}
+
+        # If the prefix is a single string (not a tuple of strings), we can
+        # do the filtering directly in the S3 API.
+        if isinstance(prefix, str):
+            kwargs['Prefix'] = prefix
+
+        while True:
+
+            # The S3 API response is a large blob of metadata.
+            # 'Contents' contains information about the listed objects.
+            resp = s3.list_objects_v2(**kwargs)
+
+            try:
+                contents = resp['Contents']
+            except KeyError:
+                return
+
+            for obj in contents:
+                key = obj['Key']
+                if key.startswith(prefix) and key.endswith(suffix):
+                    yield obj
+
+            # The S3 API is paginated, returning up to 1000 keys at a time.
+            # Pass the continuation token into the next response, until we
+            # reach the final page (when this field is missing).
+            try:
+                kwargs['ContinuationToken'] = resp['NextContinuationToken']
+            except KeyError:
+                break
+
+    def get_matching_s3_keys(bucket, prefix='', suffix=''):
+        """
+        Generate the keys in an S3 bucket.
+
+        :param bucket: Name of the S3 bucket.
+        :param prefix: Only fetch keys that start with this prefix (optional).
+        :param suffix: Only fetch keys that end with this suffix (optional).
+
+        From https://alexwlchan.net/2018/01/listing-s3-keys-redux/
+        """
+        for obj in get_matching_s3_objects(bucket, prefix, suffix):
+            yield obj['Key']
+
     def create_new_source(self, source_id, source_label):
         """Create a new S3 source folder"""
 
@@ -169,26 +225,9 @@ class S3InputManager(InputManager):
         print("not implemented yet")
 
 
-
 if __name__ == "__main__":
-    # Create object using factory.
-    # config=Config(os.getcwd())
-    # config.add_option_inputs_root('s3')
-    # config.add_option_inputs_S3bucket('js-dpp-lab-ds1-data-dev')
-    # obj = InputManagerFactory.create(config)
-    # obj.create_new_source(45,'transactions')
-    dev = boto3.session.Session(profile_name='dpp1')
-    client = dev.client('s3')
-    result = client.list_objects(
-        Bucket="js-dpp-lab-ds1-data-dev",
-        Prefix="projects/")
-    print(result)
-    print("")
-    dev = boto3.session.Session(profile_name='dpp1')
-    client = dev.client('s3')
+    config = Config(os.getcwd())
+    test = S3InputManager(config)
 
-    response = client.put_object(
-        Bucket='js-dpp-lab-ds1-data-dev',
-        Body='',
-        Key='projects/test_folder/',
-        ServerSideEncryption='AES256')
+    for key in test.get_matching_s3_keys("js-dpp-lab-ds1-data-dev", "projects"):
+        print(key)
